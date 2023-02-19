8a9e87c1a64c0e4bd12352194e598bede14052f3
momeppkt <peeranut32@gmail.com>
2023-02-19 18:33:47 +0700

    feat(credentials): add method to use only portal config without credentials stored and auto connection

--- src/ESPConnect.h
+++ src/ESPConnect.h
@@ -1,44 +1,42 @@
 #ifndef ESPConnect_h
 #define ESPConnect_h
 
-
-#include <functional>
 #include <Arduino.h>
+#include <functional>
 
 #if defined(ESP8266)
-  #include "ESP8266WiFi.h"
-  #include "WiFiClient.h"
-  #include "ESPAsyncTCP.h"
+#include "ESP8266WiFi.h"
+#include "ESPAsyncTCP.h"
+#include "WiFiClient.h"
 #elif defined(ESP32)
-  #include "WiFi.h"
-  #include "WiFiClient.h"
-  #include "AsyncTCP.h"
-  #include "Preferences.h"
+#include "AsyncTCP.h"
+#include "Preferences.h"
+#include "WiFi.h"
+#include "WiFiClient.h"
 #endif
 
-#include "ESPAsyncWebServer.h"
 #include "DNSServer.h"
+#include "ESPAsyncWebServer.h"
 #include "espconnect_webpage.h"
 
 /* Library Default Settings */
-#define ESPCONNECT_DEBUG 1
+#define ESPCONNECT_DEBUG           1
 
 #define DEFAULT_CONNECTION_TIMEOUT 30000
-#define DEFAULT_PORTAL_TIMEOUT 180000
-
+#define DEFAULT_PORTAL_TIMEOUT     180000
 
 #if ESPCONNECT_DEBUG == 1
-  #define ESPCONNECT_SERIAL(x) Serial.print("[ESPConnect]["+String(millis())+"] "+x)
+#define ESPCONNECT_SERIAL(fmt, ...) log_i("[ESPConnect][%lu] " fmt, millis(), ##__VA_ARGS__)
 #else
-  #define ESPCONNECT_SERIAL(x)
+#define ESPCONNECT_SERIAL(x)
 #endif
 
+class ESPConnectClass
+{
 
-class ESPConnectClass {
-
-  private:
-    DNSServer* _dns = nullptr;
-    AsyncWebServer* _server = nullptr;
+private:
+    DNSServer *_dns = nullptr;
+    AsyncWebServer *_server = nullptr;
 
     String _auto_connect_ssid = "";
     String _auto_connect_password = "";
@@ -47,22 +45,25 @@ class ESPConnectClass {
     String _sta_ssid = "";
     String _sta_password = "";
 
-  private:
+private:
     void load_sta_credentials();
+    int save_sta_credentials(const String &ssid, const String &password);
 
     // Start Captive portal
-    bool start_portal();
+    bool start_portal(bool isStoreCredential = true);
 
-
-  public:
+public:
     // Check if ESPConnect was configured before
     bool isConfigured();
 
     // Set Custom AP
-    void autoConnect(const char* ssid, const char* password = "", unsigned long timeout = DEFAULT_PORTAL_TIMEOUT);
+    void autoConnect(const char *ssid, const char *password = "", unsigned long timeout = DEFAULT_PORTAL_TIMEOUT);
 
     // Connect to Saved WiFi Credentials
-    bool begin(AsyncWebServer* server, unsigned long timeout = DEFAULT_CONNECTION_TIMEOUT);
+    bool begin(AsyncWebServer *server, unsigned long timeout = DEFAULT_CONNECTION_TIMEOUT);
+
+    // Manually start portal config
+    bool startPortal(AsyncWebServer *server);
 
     // Erase Saved WiFi Credentials
     bool erase();
